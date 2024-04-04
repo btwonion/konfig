59880d185a25396506855e2c3de76343a38cf001
btwonion <btwonion@gmx.de>
2024-04-04 20:54:48 +0200

    add Serializable annotation to generic of ConfigFile

--- src/main/kotlin/dev/nyon/konfig/config/ConfigFile.kt
+++ src/main/kotlin/dev/nyon/konfig/config/ConfigFile.kt
@@ -1,8 +1,9 @@
 package dev.nyon.konfig.config
 
 import dev.nyon.konfig.internal.InternalKonfigApi
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlin.reflect.KClass
 
 @InternalKonfigApi
-data class ConfigFile<T : Any>(val type: KClass<T>, val settings: ConfigSettings, val defaultInstance: T, val json: Json)
+data class ConfigFile<T : @Serializable Any>(val type: KClass<T>, val settings: ConfigSettings, val defaultInstance: T, val json: Json)
